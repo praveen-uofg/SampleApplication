a7cb5d1f99a115331b77ea4147b1f3504065a933
Praveen <praveen.nitap@gmail.com>
2015-10-01 01:59:04 +0530

    chnge for current location async task

--- app/src/main/java/com/example/praveen/sampleapplication/MainActivity.java
+++ app/src/main/java/com/example/praveen/sampleapplication/MainActivity.java
@@ -201,6 +201,7 @@ public class MainActivity extends Activity implements JsonResultSetInterface,Loc
                     e.printStackTrace();
                 }*/
                 CurrentLocationAsyncTask currentLocationAsyncTask = new CurrentLocationAsyncTask(this,mCurrentLat,mCurrentLon);
+                currentLocationAsyncTask.jsonResult = this;
                 currentLocationAsyncTask.execute();
             }
         }
